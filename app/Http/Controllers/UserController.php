ae37c2373993ee8b5c19cbf95819b66a04e3e669
SaraBarrett <sara.c.mon@gmail.com>
2025-12-09 12:00:56 +0000

    all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,14 +7,30 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function addUser(){
+
         $pageAdmin = 'António';
         return view('users.add_user', compact('pageAdmin'));
     }
 
     public function allUsers(){
-        $cesaeEmail = 'info@cesae.pt';
+        $cesaeInfo =
+        ['name' =>'cesae',
+        'email' => 'cesae@gmail.com',
+        'address' =>' rua do cesae'];
 
-        return view('users.all_users',compact('cesaeEmail'));
+        //simular consulta à base de dados
+        $students = [
+            ['name' =>'Rafael', 'email' =>'Rafael@gmail.com'],
+            ['name' =>'Mafalda', 'email' =>'Mafalda@gmail.com'],
+            ['name' =>'Luís', 'email' =>'Luís@gmail.com'],
+            ['name' =>'Leandro', 'email' =>'Leandro@gmail.com'],
+        ];
+
+        //dd($cesaeInfo['name']);
+
+
+
+        return view('users.all_users',compact('cesaeInfo', 'students'));
     }
 
 }
--- resources/views/users/all_users.blade.php
+++ resources/views/users/all_users.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main_layout')
 @section('content')
-<p>O email de contacto, caso detecte erros é {{$cesaeEmail}} </p>
+<p>O email de contacto, caso detecte erros é {{$cesaeInfo['email']}}, na {{$cesaeInfo['address']}} </p>
     <h5>Lista de todos os users</h5>
+    <ul>
+        @foreach ($students as $student)
+        <li>{{$student['name']}}  e o email é {{$student['email']}} </li>
+        @endforeach
+    </ul>
 @endsection
